ddeb28c765a53a086fcd1404bd7122181ace637e
macklehatton <macklehatton@gmail.com>
2022-01-29 15:25:17 -0600

    Collapsing block

---
+++ Assets/Scripts/Obstacles/CollapsingBlock.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CollapsingBlock : MonoBehaviour
+{
+    [SerializeField]
+    private bool humanCollapses;
+    [SerializeField]
+    private bool robotCollapses;
+    [SerializeField]
+    private float respawnTime;
+    [SerializeField]
+    private MovementMode mode;
+
+    private Collider2D blockCollider;
+    private TilemapRenderer renderer;
+
+    private bool collapsed;
+    private float timeCollapsed;
+
+    private void Start()
+    {
+        blockCollider = GetComponent<Collider2D>();
+        renderer = GetComponent<TilemapRenderer>();
+    }
+
+    public void Update()
+    {
+        if (collapsed)
+        {
+            timeCollapsed += Time.deltaTime;
+
+            if (timeCollapsed >= respawnTime)
+            {
+                Respawn();
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !mode.robotMode)
+        {
+            Collapse();
+        }
+    }
+
+    public void Collapse()
+    {
+        renderer.enabled = false;
+        collapsed = true;
+        blockCollider.enabled = false;
+    }
+
+    public void Respawn()
+    {
+        renderer.enabled = true;
+        timeCollapsed = 0.0f;
+        collapsed = false;
+        blockCollider.enabled = true;
+    }
+}
