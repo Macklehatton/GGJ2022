29598ce2853de2102bfddf7a3b8d4a5e905e47d9
macklehatton <macklehatton@gmail.com>
2022-01-30 09:40:22 -0600

    Working on animation

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerMode mode;
+    [SerializeField]
+    private Animator humanAnimator;
+    [SerializeField]
+    private Animator robotAnimator;
+
+    private float speed;
+    private Vector3 lastPosition;
+
+    private void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    void Update()
+    {
+        speed = (transform.position - lastPosition).magnitude;
+        lastPosition = transform.position;
+
+        if (!mode.RobotMode)
+        {
+            HandleHumanMode();
+        }
+        else
+        {
+            HandleRobotMode();
+        }
+    }
+
+    private void HandleHumanMode()
+    {
+        if (speed > 0.1f)
+        {
+            humanAnimator.SetTrigger("humanWalk");
+        }
+        else
+        {
+            humanAnimator.SetTrigger("humanIdle");
+        }
+    }
+
+    private void HandleRobotMode()
+    {
+        if (speed > 0.1f)
+        {
+            robotAnimator.SetTrigger("robotWalk");
+        }
+        else
+        {
+            robotAnimator.SetTrigger("robotIdle");
+        }
+    }
+}
